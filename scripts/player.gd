7162d34709dd3190bc237edfd4d9ba6e5a05edb1
mateussousadeoliveira <mateussousade29042010@gmail.com>
2026-08-13 08:14:25 -0300

    Added WASD controls and other keys

--- scripts/player.gd
+++ scripts/player.gd
@@ -33,7 +33,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 2
 
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
